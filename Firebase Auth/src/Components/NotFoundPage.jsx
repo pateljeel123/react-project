dcef248fd8707ac815fa972585b52409df79de1c
unknown <jeelpatel98796@gmail.com>
2024-08-21 13:57:28 +0530

    updated

---
+++ Firebase Auth/src/Components/NotFoundPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotFoundPage = () => {
+  return (
+    <div className='banner d-flex justify-content-center align-items-center'>
+      <h1>Page Not Found</h1>
+    </div>
+  )
+}
+
+export default NotFoundPage
